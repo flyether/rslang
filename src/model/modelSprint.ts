fcc62e9bbf9c2f5dd127bbc4b79b368fa5183ca8
Darina992 <darina992@gmail.com>
2022-08-17 15:25:42 +0300

    feat:add scoring

--- src/model/modelSprint.ts
+++ src/model/modelSprint.ts
@@ -28,6 +28,16 @@ export class ModuleModel {
 
   timerId !: ReturnType<typeof setInterval>;
 
+  score = 0;
+
+  scoreForRightAnswer = 10;
+
+  arrayOfQuestion: string[] = [];
+
+  arrayOfAnswers: boolean[] = [];
+
+  amountOfRightAnswers = 0;
+
   init(view: ModuleView):void {
     this.myModuleView = view;
     this.fillAndSortPages();
@@ -46,10 +56,10 @@ export class ModuleModel {
     const res = await api.getWords(this.level, this.pages[this.activePageNumber]) as IWord[];
     this.active20Words = Array.from(res);
     console.log(this.active20Words);
-    if (this.activePageNumber === 0) { this.prepearFirsttWord(); }
+    if (this.activePageNumber === 0) { this.prepearFirstWord(); }
   }
 
-  prepearFirsttWord() {
+  prepearFirstWord() {
     const translation = this.generateTranslation();
     const { word } = this.active20Words[this.activeWordNumber];
     this.myModuleView.renderWord(word, translation);
@@ -85,11 +95,15 @@ export class ModuleModel {
     const rightTranslate:string = this.active20Words[this.randomTranslationNumber].wordTranslate;
     if ((translate === rightTranslate) === answer) {
       console.log(true);
-      this.audioRight = document.querySelector('#audio__right') as HTMLAudioElement;
-      console.log(this.audioRight);
+      this.arrayOfAnswers.push(true);
+      this.analyzeTrueAnswer();
+      // this.audioRight = document.querySelector('#audio__right') as HTMLAudioElement;
     } else {
       console.log(false);
+      this.arrayOfAnswers.push(false);
+      this.analyzeFalseAnswer();
     }
+    console.log(this.arrayOfAnswers);
   }
 
   setTimer = (timerElem: HTMLElement) => {
@@ -105,6 +119,40 @@ export class ModuleModel {
     };
     this.timerId = setInterval(tick, 1000);
   };
+
+  countScore() {
+    this.score += this.scoreForRightAnswer;
+    if (this.scoreForRightAnswer === 80) {
+      this.myModuleView.renderScore(String(this.score), true);
+    } else {
+      this.myModuleView.renderScore(String(this.score));
+    }
+  }
+
+  analyzeFalseAnswer() {
+    this.amountOfRightAnswers = 0;
+    this.myModuleView.clearSprintCount();
+    this.scoreForRightAnswer = 10;
+    this.myModuleView.renderCountText(this.scoreForRightAnswer);
+  }
+
+  analyzeTrueAnswer() {
+    console.log(this.amountOfRightAnswers);
+    this.amountOfRightAnswers += 1;
+    this.countScore();
+    if (this.amountOfRightAnswers === 3 && this.scoreForRightAnswer !== 80) {
+      this.getBiggerScore();
+    }
+    if (this.amountOfRightAnswers === 4 && this.scoreForRightAnswer !== 80) {
+      this.myModuleView.returnToOneSprintCount();
+      this.amountOfRightAnswers = 1;
+    }
+  }
+
+  getBiggerScore() {
+    this.scoreForRightAnswer = (this.scoreForRightAnswer === 80) ? this.scoreForRightAnswer : this.scoreForRightAnswer *= 2;
+    this.myModuleView.renderCountText(this.scoreForRightAnswer);
+  }
 
   stopGame() {
     console.log('stop');
--- src/view/viewSprint.ts
+++ src/view/viewSprint.ts
@@ -1,6 +1,7 @@
 /* eslint-disable import/no-cycle */
 /* eslint-disable class-methods-use-this */
 import { Routes } from '../types/types';
+import trueImg from '../assets/true.png';
 
 export class ModuleView {
   myModuleContainer!:HTMLElement;
@@ -11,7 +12,11 @@ export class ModuleView {
 
   sprintTimer: HTMLElement = document.querySelector('.sprint__timer') as HTMLElement;
 
-  sprintScore !: HTMLElement;
+  sprintScore: HTMLElement = document.querySelector('.sprint__score') as HTMLElement;
+
+  sprintCount: HTMLElement = document.querySelector('.title__count') as HTMLElement;
+
+  sprintCountText: HTMLElement = document.querySelector('.title__text') as HTMLElement;
 
   questionWord:HTMLElement = document.querySelector('.question__word') as HTMLElement;
 
@@ -29,6 +34,27 @@ export class ModuleView {
 
   renderTimer(timer:number, timerElem:HTMLElement):void {
     timerElem.textContent = String(timer);
+  }
+
+  renderScore(score:string, maxScore?:boolean) {
+    this.sprintScore.textContent = score;
+    if (maxScore) {
+      this.sprintCount.innerHTML = `<img src="${trueImg}" alt="img">`;
+    } else {
+      this.sprintCount.innerHTML += `<img src="${trueImg}" alt="img">`;
+    }
+  }
+
+  clearSprintCount() {
+    this.sprintCount.innerHTML = '';
+  }
+
+  returnToOneSprintCount() {
+    this.sprintCount.innerHTML = `<img src="${trueImg}" alt="img">`;
+  }
+
+  renderCountText(n:number) {
+    this.sprintCountText.innerHTML = `+ ${n} очков за слово`;
   }
 }
 
