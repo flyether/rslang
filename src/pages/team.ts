287b94704c7d62652749be4f685c83c27ebf901d
Darina992 <darina992@gmail.com>
2022-08-18 16:05:48 +0300

    feat: team-page

--- src/pages/team.ts
+++ src/pages/team.ts
@@ -1,12 +1,57 @@
 import Header from '../components/header';
 import Footer from '../components/footer';
 
+import foto1 from '../assets/github-img/flyether.jpg';
+import foto2 from '../assets/github-img/AlenaDoz.jpg';
+import foto3 from '../assets/github-img/darina992.jpg';
+
 const AboutTeamPage = {
   classname: 'team-page',
   render(): string {
     return `
     ${Header.render()}
-    <div class = '${this.classname}-conteiner'></div>
+    <div class = '${this.classname}-conteiner'>
+      <p class='ourteam__title'>Наша команда</p>
+      <div class='ourteam__conteiner'>
+        <div class='ourteam'>
+          <div class='ourteam__github'>
+            <img src='${foto1}' alt='foto' class='ourteam__fotos'>
+            <a href="https://github.com/flyether" class="github-link">flyether</a>
+          </div>
+          <span class='ourteam__teamlead'>TeamLead</span>
+          <ul class="development__list">
+            <li class="development__item">Backend</li>
+            <li class="development__item">Авторизация</li>
+            <li class="development__item">Мини-игра 'Аудиовызов'</li>
+            <li class="development__item">...</li>
+          </ul>
+        </div>
+        <div class='ourteam'>
+          <div class='ourteam__github'>
+            <img src='${foto2}' alt='foto' class='ourteam__fotos'>
+            <a href="https://github.com/AlenaDoz" class="github-link">AlenaDoz</a>
+          </div>
+          <ul class="development__list">
+            <li class="development__item">Электронный учебник</li>
+            <li class="development__item">...</li>
+            <li class="development__item">...</li>
+            <li class="development__item">...</li>
+          </ul>
+        </div>
+        <div class='ourteam'>
+          <div class='ourteam__github'>
+            <img src='${foto3}' alt='foto' class='ourteam__fotos'>
+            <a href="https://github.com/Darina992" class="github-link">Darina992</a>
+          </div>
+          <ul class="development__list">
+            <li class="development__item">Главная страница</li>
+            <li class="development__item">Мини-игра 'Спринт'</li>
+            <li class="development__item">...</li>
+            <li class="development__item">...</li>
+          </ul>
+        </div>
+      </div>
+    </div>
     ${Footer.render()}
     `;
   },
