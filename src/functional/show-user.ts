5c2371ba3b1232c8fa091bdbe72c387255b4072b
Darina992 <darina992@gmail.com>
2022-08-18 11:02:59 +0300

    fix: conflicts

--- src/functional/show-user.ts
+++ src/functional/show-user.ts
@@ -4,10 +4,6 @@
 
 import { storage } from './storage';
 
-<<<<<<< HEAD
-const user = storage.name;
-const userShow = `<div class="user-show"> ${user} </div>`;
-=======
 export default function showUser(ret:boolean) :string | undefined {
   let className = 'authorization';
   let context = 'Войти';
@@ -16,7 +12,6 @@ export default function showUser(ret:boolean) :string | undefined {
     className = 'exit_btn';
   }
   const html = `<div class="user-show"> ${storage.user?.name} <button class=${className}>${context}</button> </div>`;
->>>>>>> develop
 
   const header = document.querySelector('.header__auth') as HTMLElement;
   if (ret) {
