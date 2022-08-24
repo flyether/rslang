0eb9936b65eb8752fca2cf20c6e5e505a276d450
AlenaDoz <alena.abunassar1@mail.ru>
2022-08-24 15:31:09 +0300

    feat:add styles for audiocall start

--- src/pages/audiocall/start-audiocall.ts
+++ src/pages/audiocall/start-audiocall.ts
@@ -11,20 +11,22 @@ export const startScrinAudioCall = {
     return /* html */` 
 
 <div class="audio-call"> 
-<p class="audio-call__text">В игре аудиовызов вамнужно выбрать правильный перевод слова. <br>
+<p class="audio-call__text">В игре аудиовызов вам нужно выбрать правильный перевод слова. <br>
 
 Чтобы играть с помощью клавиатуры, используй клавиши
 1, 2, 3, 4, 5 - чтобы дать ответ,
 space - для воспроизведения звука,
 enter - чтобы пропустить вопрос,
-стрелка-вправо - чтобы перейти к следующему вопросу.<br> <br>
-Выбери уровень сложности:</p>
+стрелка-вправо - чтобы перейти к следующему вопросу.</p>
+<p>Выбери уровень сложности:</p>
+<div class="audio-call__btns">
 <button id="level1" type="button"  class="btn-level"><a href="${this.hashes.audiocallGame}" class="menu__main menu__item_active">Уровень 1</a></button>
 <button id="level2" type="button"  class="btn-level">Уровень 2</button>
 <button id="level3" type="button"  class="btn-level">Уровень 3</button>
 <button id="level4" type="button"  class="btn-level">Уровень 4</button>
 <button id="level5" type="button"  class="btn-level">Уровень 5</button>
 <button id="level6" type="button"  class="btn-level">Уровень 6</button>
+</div>
 </div>
 `;
   },
