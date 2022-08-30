778b60218c4852ba1f777db56125db4b530f5f12
AlenaDoz <alena.abunassar1@mail.ru>
2022-08-30 16:51:47 +0300

    feat:add styles for audiocall

--- src/pages/audiocall/main.ts
+++ src/pages/audiocall/main.ts
@@ -12,7 +12,7 @@ const AudiocallGame = {
     support.printBtnString();
     return `
        <div class = 'container'> 
-       <a class="go-main " href='#main' >Вернуться <br> на главную</a> 
+       <a class="go-main " href='#main' >Вернуться на главную</a> 
          <div class = 'game'> ${gameArea} </div>
          
          
