5c91d582677eb78f85e40605223f915f8c2dae35
Darina992 <darina992@gmail.com>
2022-09-03 11:16:26 +0200

    fix: statistics highlightning for audiocall

--- src/utils/func.ts
+++ src/utils/func.ts
@@ -2,7 +2,7 @@ export function randomInteger(min:number, max:number):number {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-export function checkUserAuthorization(): string | undefined {
+export function checkUserAuthorization(): string {
   const user = localStorage.getItem('user');
   if (user) {
     const userObj = JSON.parse(user);
